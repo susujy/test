1fc4e31214f4350f39a401f8997f40f9a5127baa
ttljcatamount <sunwt1120@163.com>
2019-06-08 20:16:47 +0800

    taotao login

--- vue_app_00/src/router.js
+++ vue_app_00/src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // 引入美妆测评主页面
 import Home from './components/test/Home'
+// 引入登录页面
+import login from './components/test/login'
 
 Vue.use(Router)
 
@@ -9,6 +11,7 @@ export default new Router({
   routes: [
     // 美妆测评主页面
     {path:'/',component:Home},
+    {path:'/login',component:login}
     // // 评论页面
     // {path:"/comment",component:comment},
     // // 分类子组件
