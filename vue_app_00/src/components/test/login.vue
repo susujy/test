f26b5ede300422685e6b6a75a64ea67648cbdda3
ttljcatamount <sunwt1120@163.com>
2019-06-09 17:51:42 +0800

    完善Login

--- vue_app_00/src/components/test/login.vue
+++ vue_app_00/src/components/test/login.vue
@@ -5,17 +5,18 @@
 	</div>
 
 	<div>
-		<input class="phone" type="text" placeholder="请输入手机号码">
-		<input class="verification_code" type="text" placeholder="请输入短信验证码">
-		<button id="btn1">获取验证码</button>
+		<input class="phone" type="text" v-model="uname" placeholder="请输入手机号码">
+		<input class="verification_code" v-model="upass" type="text" :placeholder="verificationCode">
+		<a href="javascript:;" id="btn1" v-show="agree">获取验证码</a>
 	</div>
 	<div>
-		<span id="span">登录即表示同意</span>
-		<a id="a1" href="javascript:;">《美妆心得用户协议》</a>
-		<button id="btn2">登录</button>
+		<span id="span" v-show="agree">登录即表示同意</span>
+		<a id="a1" href="javascript:;" v-show="agree">《美妆心得用户协议》</a>
+		<a id="forget" href="javascript:;" v-show="forget">忘记密码</a>
+		<button id="btn2" @click="go">登录</button>
 	</div>
 	<div>
-		<a id="a3" href="javascript:;">账号登录</a>
+		<a id="a3" href="javascript:;" @click="openToast">{{login}}</a>
 	</div>
 	<div>
 		<hr id="hr1">
@@ -33,7 +34,61 @@
 
 <script>
 export default {
-
+	data(){
+		return {
+			verificationCode:"请输入6位短信验证码",
+			isUserAccount : true,
+			login:"账号登录",
+			agree:true,
+			forget:false,
+			uname:"",
+			upass:""
+		}
+	},
+	methods:{
+		openToast(){
+			//如果当前是以账户登录
+			if(this.isUserAccount){
+				//4.修改提示消息 手机号/邮箱/会员名
+				this.verificationCode="请输入6位短信验证码",
+				this.login="账号登录",
+				this.isUserAccount=false
+				this.agree=true
+				this.forget=false
+			}else{
+				//5.否则修改提示 请输入手机号码
+				this.verificationCode="请输入6-16位密码",
+				this.login="验证码登录",
+				this.isUserAccount=true,
+				this.agree=false
+				this.forget=true
+				//6.isUserAccount切换
+			}
+		},
+		go(){
+			//1.获取用户输入用户名   3~10
+			var n= this.uname
+			var p= this.upass
+			console.log(n,p)
+			//3.创建两个正则表达式
+			var nreg=/^1(3|4|5|6|7|8|9)\d{9}$/ig
+			var preg=/^[a-zA-Z0-9]{3,8}$/ig
+			//4.判断用户名格式不正确 提示出错
+			if(!nreg.test(n)){
+			this.$toast("手机号码格式错误");
+			return;
+			}
+			//5.判断密码格式不正确   提示出错
+			else if(!preg.test(p)){
+			this.$toast("密码格式错误")
+			return;
+			}
+			//6.将用户名和密码发送   ajax请求
+			//7.并且获取服务器返回结果
+			//8.返回失败            提示
+			//9.返回成功 
+		}
+	}
 }
 </script>
 <style scoped>
@@ -46,22 +101,21 @@ export default {
 	width:75%;
 	border-radius: 5px;
 	margin-left:45px;
+	margin-top:28px;
 }
 /* 验证码 */
 .verification_code{
-		width:50%;
+		width:75%;
 		border-radius: 5px;
 		margin-left:45px;
 		margin-right:0px;
-		border-right:0;
 	}
 #btn1{
-	/* border:1px solid #f00; */
-	margin-right:5px;
-	padding:7px 3px;
-	border-left:0;
 	color:#dd4d4d;
 	font-size:17px;
+	position: absolute;
+	top:191px;
+	left:232px;
 }
 /* 协议 */
 #span{
@@ -72,6 +126,11 @@ export default {
 	font-size:14px;
 	text-decoration: none;
 	color:#dd4d4d;
+}
+#forget{
+	font-size:14px;
+	color:#000;
+	margin-left:260px;
 }
 /* 登录 */
 #btn2{
@@ -103,13 +162,12 @@ export default {
 	width:10%;
 	display: inline-block;
 	margin-top:120px;
-	margin-right:110px;
-	float: right;
+	margin-left:70px;
 }
 #span1{
 	font-size:13px;
 	position: absolute;
-	top:445px;
+	top:473px;
 	left:160px;
 }
 /* 快捷登录 */
