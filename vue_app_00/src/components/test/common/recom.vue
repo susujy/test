935ebce91116d11f425fd6ae12ddc0de16a5cde0
susujy <sujing_jy1209@163.com>
2019-06-14 17:18:07 +0800

    首页面板完善

--- vue_app_00/src/components/test/common/recom.vue
+++ vue_app_00/src/components/test/common/recom.vue
@@ -16,15 +16,17 @@
 export default {
     data() {
         return {
-      
             list:[{lid:1,url:require('../../../assets/list1.jpg'),dec:"韩国进口 JMsolution 玫瑰粉色防晒棒 全身水润防晒霜防晒棒 spf50"},
             {lid:2,url:require('../../../assets/list2.jpg'),dec:"韩国 RECIPE莱斯壁水晶防晒喷雾50ml 小S同款SPF50+防紫外线防水防汗"},
             {lid:3,url:require('../../../assets/list3.jpg'),dec:"曼秀雷敦 （Mentholatum） 新碧轻透润色 防晒隔离乳SPF50+PA+++30g 提亮肤色清爽隔离持久 妆前打底不脱妆"},
              {lid:4,url:require('../../../assets/list4.jpg'),dec:"美国进口 Coppertone（水宝宝）纯净防晒乳 SPF50 177ml/瓶 （温和滋润 防水隔离 轻薄保湿）"}]
         }
     },
+    props:{    //接收父组件数据
+    lists:Array,
+  }
 }
-// console.log(list)
+// console.log(lists)
 </script>
 <style scoped>
     /* div#list{
